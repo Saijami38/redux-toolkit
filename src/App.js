a482eeed393d1fcb5a7701568975c2cd370f0d61
saijami <saijami@unosimple.com>
2023-08-11 11:50:59 +0530

    UserDetails code added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+import UserDetails from "./Components/UserDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserDetails />} />
+        <Route path="/redux-toolkit" element={<UserDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
