0c1483e0e3f998b65ee6b7878f04532d24b51e47
saijami <saijami@unosimple.com>
2023-08-14 15:46:03 +0530

    axios multiple api call

--- src/store/slice/TodoSlice.js
+++ src/store/slice/TodoSlice.js
@@ -1,8 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const fetchTodos = createAsyncThunk("fetchTodos", async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  return response.json();
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+  return response.data;
+});
+
+export const fetchOtherData = createAsyncThunk("fetchOtherData", async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/photos"
+  );
+  return response.data;
 });
 
 const todoSlice = createSlice({
@@ -11,18 +21,28 @@ const todoSlice = createSlice({
     isLoading: false,
     data: [],
   },
-  extraReducers: (bulider) => {
-    bulider.addCase(fetchTodos.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.data = action.payload;
-    });
-    bulider.addCase(fetchTodos.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    bulider.addCase(fetchTodos.rejected, (state, action) => {
-      console.log("Error", action.payload);
-      state.isLoading = true;
-    });
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchTodos.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchTodos.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(fetchOtherData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchOtherData.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchOtherData.rejected, (state) => {
+        state.isLoading = false;
+      });
   },
 });
 
