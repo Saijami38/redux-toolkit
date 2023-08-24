56547f65cc5e0d6d865e7dd56f34597029b7dca5
saijami <saijami@unosimple.com>
2023-08-24 16:19:54 +0530

    Modal Table Added in QR Component

--- src/Components/QrApp.js
+++ src/Components/QrApp.js
@@ -1,16 +1,53 @@
-import React from "react";
-import { Input, Col, Row, Space, Button } from "antd";
+import React, { useState, useEffect } from "react";
+import { Input, Col, Row, Space, Button, Modal, Table } from "antd";
 import QRCode from "react-qr-code";
 
 import { useDispatch, useSelector } from "react-redux";
 import { setQR, resetQR } from "../store/slice/QrSlice";
 
 function QrApp() {
+  const [showModal, setShowModal] = useState(false);
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    setUserData(fetchedData?.data);
+  }, []);
+
   const dispatch = useDispatch();
   const data = useSelector((state) => state.QR);
+  const fetchedData = useSelector((state) => state.todo);
+  console.log(fetchedData?.data, "fetchedData");
+  console.log(userData, "userData");
+
+  const columns = [
+    {
+      title: "S.No",
+      dataIndex: "id",
+    },
+
+    {
+      title: "title",
+      dataIndex: "title",
+    },
+    {
+      title: "UserId",
+      dataIndex: "userId",
+    },
+    {
+      title: "Status",
+      dataIndex: "completed",
+    },
+  ];
+
   return (
     <center>
       <div style={{ margin: "2em" }}>
+        <Row justify={"start"}>
+          <Col>
+            <Button onClick={() => window.history.go(-1)}>Back</Button>
+          </Col>
+        </Row>
+
         <Row justify={"space-around"}>
           <Col>
             <b className="add-names">Enter Text</b>
@@ -52,6 +89,36 @@ function QrApp() {
           {data}
         </a>
       </div>
+      <hr />
+      <div style={{ margin: "2em" }}>
+        <Row justify={"space-around"}>
+          <Button
+            style={{
+              backgroundColor: "#abc4ff",
+              color: "white",
+              border: "none",
+              margin: "5px",
+              borderRadius: "5px",
+              transition: "background-color 0.5 ease",
+            }}
+            onClick={() => {
+              setShowModal(true);
+            }}
+          >
+            Show Data
+          </Button>
+        </Row>
+      </div>
+      <Modal
+        title="Fetched Data "
+        open={showModal}
+        footer={false}
+        onCancel={() => {
+          setShowModal(false);
+        }}
+      >
+        <Table dataSource={userData} columns={columns} />
+      </Modal>
     </center>
   );
 }
