fbc5f9112a324d1508982fa51dcd352b2d8e56ab
saijami <saijami@unosimple.com>
2023-08-25 12:44:49 +0530

    Moives Responsive added

--- src/Components/Moives.js
+++ src/Components/Moives.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Button } from "antd";
+import { Card, Col, Row, Button } from "antd";
 import axios from "axios";
 
 function Movies() {
@@ -50,38 +50,50 @@ function Movies() {
           Show Random People
         </Button>
       </center>
-      <Col>
+      <Row>
         {moivesData.length > 0
           ? moivesData.map((e) => (
               <>
-                <Card
-                  key={e?.primaryName}
-                  style={{
-                    padding: "20px",
-                    borderRadius: "5px",
-                    margin: "15px",
-                    backgroundColor: "#cbf0f8",
-                  }}
+                <Col
+                  xxl={{ span: 8 }}
+                  xl={{ span: 8 }}
+                  lg={{ span: 8 }}
+                  md={{ span: 12 }}
+                  sm={{ span: 24 }}
+                  xs={{ span: 24 }}
                 >
-                  <h4>Name : {e?.primaryName}</h4>
-                  <h4
+                  <Card
+                    key={e?.primaryName}
                     style={{
-                      textTransform: "capitalize",
+                      padding: "20px",
+                      borderRadius: "5px",
+                      margin: "15px",
+                      backgroundColor: "#cbf0f8",
                     }}
                   >
-                    Profession :{" "}
-                    <span
-                      style={{ textOverflow: "ellipsis", whiteSpace: "nowrap" }}
+                    <h4>Name : {e?.primaryName}</h4>
+                    <h4
+                      style={{
+                        textTransform: "capitalize",
+                      }}
                     >
-                      {e?.primaryProfession ? e?.primaryProfession : "N/A"}
-                    </span>
-                  </h4>
-                </Card>
+                      Profession :{" "}
+                      <span
+                        style={{
+                          textOverflow: "ellipsis",
+                          whiteSpace: "nowrap",
+                        }}
+                      >
+                        {e?.primaryProfession ? e?.primaryProfession : "N/A"}
+                      </span>
+                    </h4>
+                  </Card>
+                </Col>
                 <br />
               </>
             ))
           : "N?A"}
-      </Col>
+      </Row>
     </div>
   );
 }
