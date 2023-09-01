929bca058bfe3c03e3906d5dd68e4a57453833f7
saijami <saijami@unosimple.com>
2023-09-01 16:53:17 +0530

    New Sortable NPM

--- src/Components/Sortable.js
+++ src/Components/Sortable.js
@@ -1,68 +1,84 @@
-import React, { useEffect } from "react";
-import { Col, Row } from "antd";
-import Sortable from "sortablejs";
+import { Card } from "antd";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-export default function SortableComponent() {
-  useEffect(() => {
-    new Sortable(document.getElementById("example2Left"), {
-      group: "shared",
-      animation: 150,
-    });
+const finalSpaceCharacters = [
+  {
+    id: "gary",
+    name: "Gary Goodspeed",
+    thumb: "/images/gary.png",
+  },
+  {
+    id: "cato",
+    name: "Little Cato",
+    thumb: "/images/cato.png",
+  },
+  {
+    id: "kvn",
+    name: "KVN",
+    thumb: "/images/kvn.png",
+  },
+  {
+    id: "mooncake",
+    name: "Mooncake",
+    thumb: "/images/mooncake.png",
+  },
+  {
+    id: "quinn",
+    name: "Quinn Ergon",
+    thumb: "/images/quinn.png",
+  },
+];
 
-    new Sortable(document.getElementById("example2Right"), {
-      group: "shared",
-      animation: 150,
-    });
-  }, []);
+export default function Sortable() {
+  const [data, setData] = useState(finalSpaceCharacters);
+  console.log(data, "data");
 
-  const leftContent = [
-    { id: 0, title: "Item 1" },
-    { id: 1, title: "Item 2" },
-    { id: 2, title: "Item 3" },
-    { id: 3, title: "Item 4" },
-    { id: 4, title: "Item 5" },
-    { id: 5, title: "Item 6" },
-    { id: 6, title: "Item 7" },
-  ];
-  const rightContent = [
-    { id: 7, title: "Item 8" },
-    { id: 8, title: "Item 9" },
-    { id: 9, title: "Item 10" },
-    { id: 10, title: "Item 11" },
-    { id: 11, title: "Item 12" },
-    { id: 12, title: "Item 13" },
-    { id: 13, title: "Item 14" },
-  ];
-
-  // http://sortablejs.github.io/Sortable/
+  const handleOnDragEnd = (result) => {
+    console.log(result, "result");
+    if (result.destination) {
+      const items = Array.from(data);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      console.log(reorderedItem, "reorderedItem");
+      items.splice(result.destination.index, 0, reorderedItem);
+      setData(items);
+    }
+  };
 
   return (
-    <center>
-      <h2>Shared Lists</h2>
-      <center>
-        <Col span={2}>
-          <Row justify={"space-between"}>
-            <Col>
-              <div id="example2Left" className="sortable-list">
-                {leftContent.map((e) => (
-                  <div key={e?.id} className="sortable-item">
-                    {e.title}
-                  </div>
-                ))}
+    <div className="App">
+      <header className="App-header">
+        <h1>Final Space Characters</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <div
+                className="characters"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {data.map(({ id, name, thumb }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <Card
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <div className="characters-thumb"></div>
+                          <p>{name}</p>
+                        </Card>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
               </div>
-            </Col>
-            <Col>
-              <div id="example2Right" className="sortable-list">
-                {rightContent.map((e) => (
-                  <div key={e?.id} className="sortable-item">
-                    {e.title}
-                  </div>
-                ))}
-              </div>
-            </Col>
-          </Row>
-        </Col>
-      </center>
-    </center>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </header>
+    </div>
   );
 }
