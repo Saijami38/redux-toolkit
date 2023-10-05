74a50fb482d1282143aa4dc3b71b96272131cf73
saijamii <saijami38@gmail.com>
2023-10-05 22:12:39 +0530

    Loading added in Fetch component

--- src/Components/ListTodo.js
+++ src/Components/ListTodo.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Table } from "antd";
+import Loading from "../Common/Loading";
 import { fetchTodos } from "../store/slice/TodoSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function ListTodo() {
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const finalData = useSelector((state) => state.todo || []);
 
@@ -30,37 +32,44 @@ function ListTodo() {
     },
   ];
 
-  if (finalData?.isLoading) {
-    return <h2 style={{ minHeight: "100dvh" }}>Loading .....</h2>;
-  }
+  useEffect(() => {
+    if (finalData?.isLoading) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [finalData?.isLoading]);
 
   return (
-    <div style={{ minHeight: "100dvh" }}>
-      <Button
-        style={{
-          backgroundColor: "#abc4ff",
-          color: "white",
-          border: "none",
-          margin: "5px",
-          borderRadius: "5px",
-          transition: "background-color 0.5s ease",
-        }}
-        onClick={() => {
-          dispatch(fetchTodos());
-        }}
-      >
-        Fetch Data
-      </Button>
+    <>
+      <div style={{ minHeight: "100dvh" }}>
+        <Button
+          style={{
+            backgroundColor: "#abc4ff",
+            color: "white",
+            border: "none",
+            margin: "5px",
+            borderRadius: "5px",
+            transition: "background-color 0.5s ease",
+          }}
+          onClick={() => {
+            dispatch(fetchTodos());
+          }}
+        >
+          Fetch Data
+        </Button>
 
-      {finalData?.isLoading === false && (
-        <Table
-          columns={columns}
-          dataSource={
-            finalData?.data?.entries.length > 0 && finalData?.data?.entries
-          }
-        />
-      )}
-    </div>
+        {finalData?.isLoading === false && (
+          <Table
+            columns={columns}
+            dataSource={
+              finalData?.data?.entries.length > 0 && finalData?.data?.entries
+            }
+          />
+        )}
+      </div>
+      <Loading enableLoading={loading} />
+    </>
   );
 }
 
