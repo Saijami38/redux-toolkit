b2386a704567b637b9d45161378cebea5a8027e3
saijamii <saijami38@gmail.com>
2023-10-17 08:30:46 +0530

    Back Button added

--- src/Components/VehicleListDetails.js
+++ src/Components/VehicleListDetails.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Card, Descriptions } from "antd";
+import Back from "../Common/Back";
 
 export default function VehicleListDetails(props) {
   const [vehicleData, setVehicleData] = useState(null);
@@ -19,25 +20,51 @@ export default function VehicleListDetails(props) {
   }, []);
 
   return (
-    <div>
-      {vehicleData ? (
-        <Card title={vehicleData.properties.name}>
-          <Descriptions title="Vehicle Details">
-            <Descriptions.Item label="Model">{vehicleData.properties.model}</Descriptions.Item>
-            <Descriptions.Item label="Class">{vehicleData.properties.vehicle_class}</Descriptions.Item>
-            <Descriptions.Item label="Manufacturer">{vehicleData.properties.manufacturer}</Descriptions.Item>
-            <Descriptions.Item label="Cost (in credits)">{vehicleData.properties.cost_in_credits}</Descriptions.Item>
-            <Descriptions.Item label="Length">{vehicleData.properties.length}</Descriptions.Item>
-            <Descriptions.Item label="Crew">{vehicleData.properties.crew}</Descriptions.Item>
-            <Descriptions.Item label="Passengers">{vehicleData.properties.passengers}</Descriptions.Item>
-            <Descriptions.Item label="Max Speed">{vehicleData.properties.max_atmosphering_speed}</Descriptions.Item>
-            <Descriptions.Item label="Cargo Capacity">{vehicleData.properties.cargo_capacity}</Descriptions.Item>
-            <Descriptions.Item label="Consumables">{vehicleData.properties.consumables}</Descriptions.Item>
-          </Descriptions>
-        </Card>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
+    <>
+      <div style={{ margin: "2em", minHeight: "100vh" }}>
+        <center>
+          <Back />
+          <br />
+          {vehicleData ? (
+            <Card title={vehicleData.properties.name}>
+              <Descriptions title="Vehicle Details">
+                <Descriptions.Item label="Model">
+                  {vehicleData.properties.model}
+                </Descriptions.Item>
+                <Descriptions.Item label="Class">
+                  {vehicleData.properties.vehicle_class}
+                </Descriptions.Item>
+                <Descriptions.Item label="Manufacturer">
+                  {vehicleData.properties.manufacturer}
+                </Descriptions.Item>
+                <Descriptions.Item label="Cost (in credits)">
+                  {vehicleData.properties.cost_in_credits}
+                </Descriptions.Item>
+                <Descriptions.Item label="Length">
+                  {vehicleData.properties.length}
+                </Descriptions.Item>
+                <Descriptions.Item label="Crew">
+                  {vehicleData.properties.crew}
+                </Descriptions.Item>
+                <Descriptions.Item label="Passengers">
+                  {vehicleData.properties.passengers}
+                </Descriptions.Item>
+                <Descriptions.Item label="Max Speed">
+                  {vehicleData.properties.max_atmosphering_speed}
+                </Descriptions.Item>
+                <Descriptions.Item label="Cargo Capacity">
+                  {vehicleData.properties.cargo_capacity}
+                </Descriptions.Item>
+                <Descriptions.Item label="Consumables">
+                  {vehicleData.properties.consumables}
+                </Descriptions.Item>
+              </Descriptions>
+            </Card>
+          ) : (
+            <div>Loading...</div>
+          )}
+        </center>
+      </div>
+    </>
   );
 }
